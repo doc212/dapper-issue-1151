6d8e072da2fd84138a966340aa9c7217d15040f1
Tuan-Tu Tran <tuantu.t@gmail.com>
2018-11-01 02:55:29 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dapper_issue_1151
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

e06e5cf73bde78e1be0020cf98eca544ca4bf68a
Tuan-Tu Tran <tuantu.t@gmail.com>
2018-11-01 03:36:56 +0100

    add some example

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dapper;
 
 namespace dapper_issue_1151
 {
@@ -6,7 +9,48 @@ namespace dapper_issue_1151
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string _CONN_STRING = "Server=localhost;Uid=root;Password=pass;Database=test";
+            using (var conn = new MySql.Data.MySqlClient.MySqlConnection(_CONN_STRING))
+            {
+                string sql = @"
+                    SELECT O.OrderId, O.DateTime, D.Product
+                    FROM Orders O
+                    INNER JOIN Detail D ON D.OrderId = O.OrderId
+                ";
+                var result1 = conn.Query<Order, Detail, Order>(sql, (order, detail) => { order.Details.Add(detail.Product); return order; }, splitOn: "Product");
+                Dump(result1);
+
+                var orderById = new Dictionary<int, Order>();
+                var result2 = conn.Query<Order, Detail, Order>(sql,
+                    (order, detail) =>
+                    {
+                        if (!orderById.TryGetValue(order.OrderId, out var o))
+                        {
+                            orderById[order.OrderId] = (o = order);
+                        }
+                        o.Details.Add(detail.Product); return o;
+                    }, splitOn: "Product")
+                    .Distinct();
+                Dump(result2);
+            }
         }
+
+        private static void Dump(object result1)
+        {
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(result1, Newtonsoft.Json.Formatting.Indented));
+        }
+    }
+
+    internal class Detail
+    {
+        public int OrderId { get; set; }
+        public string Product { get; set; }
+    }
+
+    internal class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime DateTime { get; set; }
+        public List<String> Details { get; set; } = new List<string>();
     }
 }

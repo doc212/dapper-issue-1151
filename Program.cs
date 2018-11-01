9db70bc8f7e21c4806d7b3ac6198c61f38fcc4c0
Tuan-Tu Tran <tuantu.t@gmail.com>
2018-11-01 03:42:14 +0100

    add comments

--- Program.cs
+++ Program.cs
@@ -12,14 +12,71 @@ namespace dapper_issue_1151
             string _CONN_STRING = "Server=localhost;Uid=root;Password=pass;Database=test";
             using (var conn = new MySql.Data.MySqlClient.MySqlConnection(_CONN_STRING))
             {
+                /*
+                db content:
+                table Orders:
+                +---------+---------------------+
+                | OrderId | DateTime            |
+                +---------+---------------------+
+                |       1 | 2018-11-01 02:08:44 |
+                |       2 | 2018-11-01 02:08:47 |
+                +---------+---------------------+
+                table Detail
+                +----+---------+------------+
+                | Id | OrderId | Product    |
+                +----+---------+------------+
+                |  1 |       1 | detail 1.1 |
+                |  2 |       1 | detall 1.2 |
+                |  3 |       2 | detail 2   |
+                +----+---------+------------+
+
+                 */
                 string sql = @"
                     SELECT O.OrderId, O.DateTime, D.Product
                     FROM Orders O
                     INNER JOIN Detail D ON D.OrderId = O.OrderId
                 ";
+                /*
+                returns this:
+                +---------+---------------------+------------+
+                | OrderId | DateTime            | Product    |
+                +---------+---------------------+------------+
+                |       1 | 2018-11-01 02:08:44 | detail 1.1 |
+                |       1 | 2018-11-01 02:08:44 | detall 1.2 |
+                |       2 | 2018-11-01 02:08:47 | detail 2   |
+                +---------+---------------------+------------+
+                 */
+
+                 //first "simple" way
                 var result1 = conn.Query<Order, Detail, Order>(sql, (order, detail) => { order.Details.Add(detail.Product); return order; }, splitOn: "Product");
                 Dump(result1);
+                /*
+                [
+                    {
+                        "OrderId": 1,
+                        "DateTime": "2018-11-01T02:08:44Z",
+                        "Details": [
+                        "detail 1.1"
+                        ]
+                    },
+                    {
+                        "OrderId": 1,
+                        "DateTime": "2018-11-01T02:08:44Z",
+                        "Details": [
+                        "detall 1.2"
+                        ]
+                    },
+                    {
+                        "OrderId": 2,
+                        "DateTime": "2018-11-01T02:08:47Z",
+                        "Details": [
+                        "detail 2"
+                        ]
+                    }
+                ]
+                 */
 
+                //2nd way: requires a dict to track orders by id
                 var orderById = new Dictionary<int, Order>();
                 var result2 = conn.Query<Order, Detail, Order>(sql,
                     (order, detail) =>
@@ -32,6 +89,25 @@ namespace dapper_issue_1151
                     }, splitOn: "Product")
                     .Distinct();
                 Dump(result2);
+                /*
+                [
+                    {
+                        "OrderId": 1,
+                        "DateTime": "2018-11-01T02:08:44Z",
+                        "Details": [
+                        "detail 1.1",
+                        "detall 1.2"
+                        ]
+                    },
+                    {
+                        "OrderId": 2,
+                        "DateTime": "2018-11-01T02:08:47Z",
+                        "Details": [
+                        "detail 2"
+                        ]
+                    }
+                ]
+                 */
             }
         }
 
